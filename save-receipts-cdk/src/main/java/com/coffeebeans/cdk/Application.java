e92dfc1c439bf29fe67440128de0d3885212f9e6
Matto <muhamadto@gmail.com>
2021-05-20 17:10:16 +1000

    renamed modules

--- save-receipts-cdk/src/main/java/com/coffeebeans/cdk/Application.java
+++ save-receipts-cdk/src/main/java/com/coffeebeans/cdk/Application.java
@@ -7,11 +7,12 @@ import software.amazon.awscdk.core.Tags;
 public final class Application {
 
   public static void main(final String... args) {
+    final String env = System.getenv("ENV");
     final App app = new App();
 
     final VpcStack vpcStack = new VpcStack(app, "CreateVPCStack");
 
-    Tags.of(vpcStack).add("env", "dev");
+    Tags.of(vpcStack).add("env", env);
 
     app.synth();
   }
